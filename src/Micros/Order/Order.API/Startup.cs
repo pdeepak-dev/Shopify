d917a8e0e46c89f8bb5dd3fc8e3b5cdd9e573de1
Deepak Pandey <pandeepak.dev@gmail.com>
2021-12-11 18:19:11 +0530

    Added Order Api

--- src/Micros/Order/Order.API/Startup.cs
+++ src/Micros/Order/Order.API/Startup.cs
@@ -1,3 +1,4 @@
+using Order.Application;
 using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -25,6 +26,8 @@ namespace Order.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Order.API", Version = "v1" });
             });
+
+            services.AddApplicationServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

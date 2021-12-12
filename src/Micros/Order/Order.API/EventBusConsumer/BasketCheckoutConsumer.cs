14912853837fccda314f1074df4ca019667c37f7
Deepak Pandey <pandeepak.dev@gmail.com>
2021-12-12 17:27:27 +0530

    Added RabbitMQ Consumer and Docker Support to Order Microservice

---
+++ src/Micros/Order/Order.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MediatR;
+using AutoMapper;
+using MassTransit;
+using System.Threading.Tasks;
+using EventBus.Messages.Events;
+using Microsoft.Extensions.Logging;
+using Order.Application.Features.Orders.Commands.CheckoutOrder;
+
+namespace Order.API.EventBusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<BasketCheckoutConsumer> _logger;
+        private readonly IMapper _mapper;
+
+        public BasketCheckoutConsumer(IMediator mediator, ILogger<BasketCheckoutConsumer> logger, IMapper mapper)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            var command = _mapper.Map<CheckoutOrderCommand>(context.Message);
+            command.Price = context.Message.TotalPrice;
+            command.Email = context.Message.EmailAddress;
+
+            var result = await _mediator.Send(command);
+
+            _logger.LogInformation("BasketCheckoutEvent consumed successfully. Created Order Id : {newOrderId}", result);
+        }
+    }
+}
---
+++ src/Micros/Order/Order.API/Mapping/OrderProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using Order.Application.Features.Orders.Commands.CheckoutOrder;
+
+namespace Order.API.Mapping
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+            => CreateMap<BasketCheckoutEvent, CheckoutOrderCommand>().ReverseMap();
+    }
+}

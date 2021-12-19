7eaf2578ba70d50d2741fc5c1067d6b53c6927a1
Deepak Pandey <deepak.pandey1@pearson.com>
2021-12-19 16:06:00 +0530

    Added Shopping Aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Services/ICatalogService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Shopping.Aggregator.Models;
+using System.Collections.Generic;
+using Shopping.Aggregator.Extensions;
+
+namespace Shopping.Aggregator.Services
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+        Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+        Task<CatalogModel> GetCatalog(string id);
+    }
+
+    public class CatalogService : ICatalogService
+    {
+        private readonly HttpClient _client;
+
+        public CatalogService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            var response = await _client.GetAsync("/api/v1/Catalog");
+            return await response.ReadContentAs<List<CatalogModel>>();
+        }
+
+        public async Task<CatalogModel> GetCatalog(string id)
+        {
+            var response = await _client.GetAsync($"/api/v1/Catalog/{id}");
+            return await response.ReadContentAs<CatalogModel>();
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        {
+            var response = await _client.GetAsync($"/api/v1/Catalog/GetProductByCategory/{category}");
+            return await response.ReadContentAs<List<CatalogModel>>();
+        }
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/IOrderService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Shopping.Aggregator.Models;
+using System.Collections.Generic;
+using Shopping.Aggregator.Extensions;
+
+namespace Shopping.Aggregator.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            var response = await _client.GetAsync($"/api/v1/Order/{userName}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
+        }
+    }
+}

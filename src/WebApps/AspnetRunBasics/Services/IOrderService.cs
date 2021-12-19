972a54ef040edd352f17adf2419344c88f07c7c3
Deepak Pandey <pandeepak.dev@gmail.com>
2021-12-19 19:35:44 +0530

    Added UI layer for Microservices

---
+++ src/WebApps/AspnetRunBasics/Services/IOrderService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using AspnetRunBasics.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace AspnetRunBasics.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            var response = await _client.GetAsync($"/Order/{userName}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
+        }
+    }
+}
